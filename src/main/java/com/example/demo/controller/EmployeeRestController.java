244da476f57eb2b2baf7d1025e32d23bd27a14b5
vunht <vunht@VID-VUNHT-L.Vingroup.local>
2019-06-11 14:11:09 +0700

    Get method

---
+++ src/main/java/com/example/demo/controller/EmployeeRestController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.Employee;
+import com.example.demo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/service")
+public class EmployeeRestController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@RequestMapping(value = "/employees", method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+	public List<Employee> getEmployee() {
+		List<Employee> list = employeeService.getAllEmployees();
+		return list;
+	}
+
+	@RequestMapping(value = "/employees{empId}", method = RequestMethod.GET, produces = {
+			MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	@ResponseBody
+	public Employee getEmployee(@PathVariable("empId") Long empId) {
+		return employeeService.getEmployee(empId);
+	}
+
+}
---
+++ src/main/java/com/example/demo/dao/EmployeeDAO.java
@@ -0,0 +1,74 @@
+package com.example.demo.dao;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.entities.Employee;
+
+@Repository
+public class EmployeeDAO {
+
+	private static final Map<Long, Employee> empMap = new HashMap<Long, Employee>();
+
+	static {
+		initEmps();
+	}
+
+	private static void initEmps() {
+		Employee emp1 = new Employee(1L, "E01", "Test 1");
+		Employee emp2 = new Employee(1L, "E02", "Test 2");
+		Employee emp3 = new Employee(1L, "E03", "Test 3");
+		empMap.put(emp1.getEmpId(), emp1);
+		empMap.put(emp2.getEmpId(), emp2);
+		empMap.put(emp3.getEmpId(), emp3);
+	}
+
+	public Long getMaxEmpId() {
+		Set<Long> keys = empMap.keySet();
+		Long max = 0L;
+		for (Long key : keys) {
+			if (key > max) {
+				max = key;
+
+			}
+		}
+		return max;
+	}
+
+	public Employee getEmployee(Long empId) {
+		return empMap.get(empId);
+	}
+
+	public Employee addEmployee(Employee empForm) {
+		Long empId = this.getMaxEmpId() + 1;
+		empForm.setEmpId(empId);
+		empMap.put(empForm.getEmpId(), empForm);
+		return empForm;
+	}
+
+	public Employee updateEmployee(Employee empForm) {
+		Employee empUpdate = this.getEmployee(empForm.getEmpId());
+		if (empUpdate != null) {
+			empUpdate.setEmpNo(empForm.getEmpNo());
+			empUpdate.setEmpName(empForm.getEmpName());
+		}
+		return empUpdate;
+	}
+
+	public void deleteEmployee(Long empId) {
+		empMap.remove(empId);
+	}
+
+	public List<Employee> getAllEmployees() {
+		Collection<Employee> collection = empMap.values();
+		List<Employee> list = new ArrayList<Employee>();
+		list.addAll(collection);
+		return list;
+	}
+}
